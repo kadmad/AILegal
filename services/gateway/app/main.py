58cba8f1e294e8dca18be19b7cef32484c631970
kadmad <145590072+kadmad@users.noreply.github.com>
2026-03-16 02:22:52 +0530

    Revert "ZOHO #TA1-T39: Update app title of fast api to AILeagal (#24)"
    This reverts commit ce5a0ac08300786c03b03df472fced0a484486bc.

--- services/gateway/app/main.py
+++ services/gateway/app/main.py
@@ -14,7 +14,7 @@ from fastapi import FastAPI
 from app.routes.upload import router
 from app.kafka.producer import init_kafka, close_kafka
 
-app = FastAPI(title="AILeagal")
+app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
