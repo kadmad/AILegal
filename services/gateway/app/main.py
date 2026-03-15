2bbafd3da04bd2e5d08cffeb26f4d7cc8861b2cb
Kaushik Devmurari <kaushikdevmurari@Kaushiks-MacBook-Air.local>
2026-03-16 02:19:25 +0530

    ZOHO #TA1-T39: Update app title of fast api to AILeagal

--- services/gateway/app/main.py
+++ services/gateway/app/main.py
@@ -14,7 +14,7 @@ from fastapi import FastAPI
 from app.routes.upload import router
 from app.kafka.producer import init_kafka, close_kafka
 
-app = FastAPI()
+app = FastAPI(title="AILeagal")
 
 @app.on_event("startup")
 async def startup_event():
